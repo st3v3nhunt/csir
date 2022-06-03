60f8d342a02849b50125efd76b733086b9d3977c
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-03 14:26:12 +0100

    feat: restructure to subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use dotenv::dotenv;
 use serde::Deserialize;
 use std::env;
 
-#[derive(Debug, Default, Parser)]
+#[derive(Debug, Parser)]
 #[clap(author = "st3v3nhunt", version, about)]
 /// CLI Sotck Info Retriever
 struct Args {
-    #[clap(forbid_empty_values = true)]
-    /// Symbol to retrieve info about
-    symbol: String,
+    #[clap(short, long, parse(from_occurrences))]
+    verbosity: usize,
+    /// Type of request to make for symbol
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Price of symbol
+    Price {
+        /// Symbol to retrieve info about
+        #[clap(forbid_empty_values = true)]
+        symbol: String,
+    },
+    /// Company info
+    Quote {
+        /// Symbol to retrieve info about
+        #[clap(forbid_empty_values = true)]
+        symbol: String,
+    },
 }
 
 #[derive(Deserialize, Debug)]
@@ -26,16 +44,21 @@ async fn main() -> Result<()> {
 
     let args = Args::parse();
     println!("{:?}", args);
-
-    let result = get_quote(&api_key, &args.symbol).await?;
-    match result {
-        Some(r) => println!("Stock '{}' has price {}.", r.symbol, r.price),
-        None => println!("No result returned for '{}'", args.symbol),
+    match args.command {
+        Commands::Price { symbol } => match get_price(&api_key, &symbol).await? {
+            Some(r) => println!("Stock '{}' has price {}.", r.symbol, r.price),
+            None => println!("No result returned for '{}'", symbol),
+        },
+        Commands::Quote { symbol } => {
+            println!("Getting company quote for {}", symbol);
+        }
     }
+
     Ok(())
 }
 
-async fn get_quote(api_key: &str, symbol: &str) -> Result<Option<ShortQuote>> {
+async fn get_price(api_key: &str, symbol: &str) -> Result<Option<ShortQuote>> {
+    println!("Getting price");
     let resp = reqwest::get(format!(
         "https://financialmodelingprep.com/api/v3/quote-short/{symbol}?apikey={api_key}",
         symbol = symbol,
