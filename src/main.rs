d5c832c85001d79ce0d8c381bd8de16d16c83980
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-03 15:21:58 +0100

    feat: add get company quote

--- src/main.rs
+++ src/main.rs
@@ -37,30 +37,63 @@ struct ShortQuote {
     price: f64,
 }
 
+fn check_env() {
+    dotenv().ok();
+    env::var("API_KEY").expect("API_KEY must be set");
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv().ok();
-    let api_key = env::var("API_KEY").expect("API_KEY must be set");
+    check_env();
 
     let args = Args::parse();
     println!("{:?}", args);
     match args.command {
-        Commands::Price { symbol } => match get_price(&api_key, &symbol).await? {
+        Commands::Price { symbol } => match get_price(&symbol).await? {
             Some(r) => println!("Stock '{}' has price {}.", r.symbol, r.price),
             None => println!("No result returned for '{}'", symbol),
         },
-        Commands::Quote { symbol } => {
-            println!("Getting company quote for {}", symbol);
-        }
+        Commands::Quote { symbol } => get_quote(&symbol).await?,
     }
 
     Ok(())
 }
 
-async fn get_price(api_key: &str, symbol: &str) -> Result<Option<ShortQuote>> {
-    println!("Getting price");
+const URL: &str = "https://financialmodelingprep.com/api/v3";
+
+async fn get_quote(symbol: &str) -> Result<()> {
+    println!("Getting company quote for {}", symbol);
+    let api_key = env::var("API_KEY")?;
     let resp = reqwest::get(format!(
-        "https://financialmodelingprep.com/api/v3/quote-short/{symbol}?apikey={api_key}",
+        "{URL}/quote/{symbol}?apikey={api_key}",
+        api_key = api_key,
+        symbol = symbol
+    ))
+    .await?;
+    println!("{:#?}", resp);
+    match resp.status() {
+        reqwest::StatusCode::OK => {
+            let data = resp.json::<Vec<ShortQuote>>().await?;
+            println!("Response data: {:?}", data);
+            let item = data.into_iter().nth(0).unwrap();
+            println!("{}", item.symbol);
+        }
+        _ => {
+            println!(
+                "Response for '{}' returned status code '{}' and was not parsable.",
+                symbol,
+                resp.status().as_str()
+            );
+        }
+    }
+    Ok(())
+}
+
+async fn get_price(symbol: &str) -> Result<Option<ShortQuote>> {
+    println!("Getting price");
+    let api_key = env::var("API_KEY")?;
+    let resp = reqwest::get(format!(
+        "{URL}/quote-short/{symbol}?apikey={api_key}",
         symbol = symbol,
         api_key = api_key
     ))
