171a28eb0efc9c773830c838ac8b438c79aad836
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-03 11:22:01 +0100

    feat: improve help info and output

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ use dotenv::dotenv;
 use serde::Deserialize;
 use std::env;
 
-#[derive(Debug, Parser)]
-struct Cli {
+#[derive(Debug, Default, Parser)]
+#[clap(author = "st3v3nhunt", version, about)]
+/// CLI Sotck Info Retriever
+struct Args {
+    #[clap(forbid_empty_values = true)]
+    /// Symbol to retrieve info about
     symbol: String,
 }
 
@@ -13,29 +17,32 @@ struct Cli {
 struct ShortQuote {
     symbol: String,
     price: f64,
-    volume: i64,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY must be set");
-    println!("API_KEY: {}", api_key);
-    let args = Cli::parse();
+
+    let args = Args::parse();
     println!("{:?}", args);
+
+    let result = get_quote(&api_key, &args.symbol).await?;
+    match result {
+        Some(r) => println!("Stock {} has price {}.", r.symbol, r.price),
+        None => println!("No result returned for {}", args.symbol),
+    }
+    Ok(())
+}
+
+async fn get_quote(api_key: &str, symbol: &str) -> Result<Option<ShortQuote>> {
     let resp = reqwest::get(format!(
         "https://financialmodelingprep.com/api/v3/quote-short/{symbol}?apikey={api_key}",
-        symbol = &args.symbol,
+        symbol = symbol,
         api_key = api_key
     ))
     .await?;
-    println!("{:?}", resp);
     let data = resp.json::<Vec<ShortQuote>>().await?;
-    println!("{:#?}", data);
-    let result = data.first().unwrap();
-    println!(
-        "Stock {} has price {}. Last volume {}",
-        result.symbol, result.price, result.volume
-    );
-    Ok(())
+    println!("Response data: {:?}", data);
+    Ok(data.into_iter().nth(0))
 }
