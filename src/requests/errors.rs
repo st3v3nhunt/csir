ec1625020ced228a4c65af1256c48cb31ed04394
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-05 22:47:40 +0100

    refactor: add test mod and anontate with cfg(test)

--- src/requests/errors.rs
+++ src/requests/errors.rs
@@ -3,24 +3,29 @@ pub fn get_error(action: &str, symbols: &Vec<String>) -> String {
     format!("unable to get {} for {}", action, symbols.join(", "))
 }
 
-#[test]
-fn test_error_message_for_single_symbol() {
-    let action = "action";
-    let symbols = &vec!["AAPL".to_string()];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let actual = get_error(action, symbols);
+    #[test]
+    fn test_error_message_for_single_symbol() {
+        let action = "action";
+        let symbols = &vec!["AAPL".to_string()];
 
-    let expected = "unable to get action for AAPL".to_string();
-    assert_eq!(expected, actual);
-}
+        let actual = get_error(action, symbols);
 
-#[test]
-fn test_error_message_for_multiple_symbols() {
-    let action = "action";
-    let symbols = &vec!["AAPL".to_string(), "TSLA".to_string()];
+        let expected = "unable to get action for AAPL".to_string();
+        assert_eq!(expected, actual);
+    }
 
-    let actual = get_error(action, symbols);
+    #[test]
+    fn test_error_message_for_multiple_symbols() {
+        let action = "action";
+        let symbols = &vec!["AAPL".to_string(), "TSLA".to_string()];
 
-    let expected = "unable to get action for AAPL, TSLA".to_string();
-    assert_eq!(expected, actual);
+        let actual = get_error(action, symbols);
+
+        let expected = "unable to get action for AAPL, TSLA".to_string();
+        assert_eq!(expected, actual);
+    }
 }
