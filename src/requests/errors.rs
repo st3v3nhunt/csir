1d44823fee19591f9909d719527a32a33a1b0bef
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-06-05 22:39:19 +0100

    feat: add tests for errors

--- src/requests/errors.rs
+++ src/requests/errors.rs
@@ -1,3 +1,26 @@
-pub fn get_error(function: &str, symbols: &Vec<String>) -> String {
-    format!("unable to get {} for {}", function, symbols.join(","))
+/// Returns error message for function with symbols concatenated.
+pub fn get_error(action: &str, symbols: &Vec<String>) -> String {
+    format!("unable to get {} for {}", action, symbols.join(", "))
+}
+
+#[test]
+fn test_error_message_for_single_symbol() {
+    let action = "action";
+    let symbols = &vec!["AAPL".to_string()];
+
+    let actual = get_error(action, symbols);
+
+    let expected = "unable to get action for AAPL".to_string();
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn test_error_message_for_multiple_symbols() {
+    let action = "action";
+    let symbols = &vec!["AAPL".to_string(), "TSLA".to_string()];
+
+    let actual = get_error(action, symbols);
+
+    let expected = "unable to get action for AAPL, TSLA".to_string();
+    assert_eq!(expected, actual);
 }
